05356a66ac544a8191b460538e10cf0295e8f04e
Michael Dickey <54130818+michaelfdickey@users.noreply.github.com>
2021-12-23 22:06:05 +0000

    initial commit module header and function specification

---
+++ func.py
@@ -0,0 +1,25 @@
+"""
+Functions to anglicize integers in the range 1 -> 999,999
+
+The primary function in this module is anglicize(). This is a great module to help understand preconditions
+
+Author: Michael Dickey
+Date: Dec 23, 2021
+
+"""
+
+def anglicize(n):
+    """
+    Returns: a string representation of the integer n in English
+
+    Examples:
+    3:      'three'
+    45:     'forty five'
+    100:    'one hundred'
+    127:    'one hundred and twenty seven'
+    1001:   'one thousand and one'
+    990899: 'nine hundred and ninety eight thousand nine hundred and ninety nine'
+
+    """
+    pass
+
